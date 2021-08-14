a4e5fbbf78280392fa6121c6a3c7f84d166efba9
Keita Ishibashi <1484keita@gmail.com>
2021-08-15 05:54:34 +0900

    add validation by react hook form

---
+++ lib/hubspot.ts
@@ -0,0 +1,46 @@
+import axios from 'axios'
+
+export const submitHubspotForm = async (email: string, firstname: string, lastname: string, msg: string) => {
+  const portalId = '20536962'
+  const formId = '23c091f6-32ab-4d82-ac44-24005e743ac2'
+  const url = `https://api.hsforms.com/submissions/v3/integration/submit/${portalId}/${formId}`
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }
+
+  try {
+    const res = await axios.post (
+      url,
+      {
+        portalId,
+        formId,
+        fields: [
+          {
+            name: 'firstname',
+            value: firstname,
+          },
+          {
+            name: 'lastname',
+            value: lastname,
+          },
+          {
+            name: 'email',
+            value: email,
+          },
+          {
+            name: 'message',
+            value: msg,
+          },
+        ],
+      },
+      config
+    );
+    return res;
+  } catch(err) {
+    const errorMessage = err.response.data || err.message;
+    console.log(errorMessage);
+  }
+}
